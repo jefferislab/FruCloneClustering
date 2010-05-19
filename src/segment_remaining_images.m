26b780d46d393ec08d1eeccf30cca12bedbd88d2
Gregory Jefferis <jefferis@gmail.com>
2010-05-19 21:20:06 +0100

    Greg's version of segment remaining images
    * adds threshold as argument
    * gives informative names to variables
    * adds comments to explain what's happening

--- src/segment_remaining_images.m
+++ src/segment_remaining_images.m
@@ -1,79 +1,76 @@
-function segment_remaining_images(input_dir,output_dir);
+function segment_remaining_images(input_dir,output_dir,threshold);
 
 % this script takes tubed images, thresholds them, and then segments them
 % the input files are XXXtubed.PIC the and output files are XXXtubed.mat.
+% The segmentation threshold defaults to 10
 
-%input directory
-tubed_dir='/Volumes/JData/JPeople/Nick/FruCloneClustering/images/';
-tubed_dir=input_dir;
-%output directory
-%output_dir='~/Projects/imageProcessing/tubed_files/';
+if nargin < 3
+	threshold = 10
+end
 
+% For testing:
+% input_dir = '/Volumes/JData/JPeople/Nick/FruCloneClustering/images/';
+% output_dir='~/Projects/imageProcessing/tubed_files/';
 
-h=dir([tubed_dir,'*-tubed.PIC']);
+% Process all input files sequentually
+input_files=dir([input_dir,'*-tubed.PIC']);
+for i=1:length(input_files)
 
+	% Trim input file name up to first underscore
+	% Apparently there are some cases of multiple images with the same
+	% stem and Nick only wanted to process one of these
+	n=find(input_files(i).name=='_',1,'first');
+	inputstem=input_files(i).name(1:n-1);
 
-for i=1:length(h)
+	processFile=1;
 
-	n=find(h(i).name=='_',1,'first');
-	name=h(i).name(1:n-1);
+	% Check if we have already generated an output file for this brain
+	output_files=dir([output_dir,'*_tubed.mat']);
+	for j=1:length(output_files)
 
-	flag=1;
+		n=find(output_files(j).name=='_',1,'first');
+		outputstem=output_files(j).name(1:n-1);
 
-	h1=dir([output_dir,'*_tubed.mat']);
-
-
-	for j=1:length(h1)
-
-		n=find(h1(j).name=='_',1,'first');
-		name1=h1(j).name(1:n-1);
-
-
-		if strcmp(name,name1)
-
-			flag=0;
+		if strcmp(inputstem,outputstem)
+			processFile=0;
 			break
-
 		end
 
 	end
 
-	h2=dir([output_dir,'*-in_progress.mat']);
+	% Check if we have already generated a lockfile for this brain
+	lock_files=dir([output_dir,'*-in_progress.mat']);
+	for j=1:length(lock_files)
 
-	for j=1:length(h2)
+		n=find(lock_files(j).name=='_',1,'first');
+		lockstem=lock_files(j).name(1:n-1);
 
-		n=find(h2(j).name=='_',1,'first');
-		name2=h2(j).name(1:n-1);
-
-		if strcmp(name,name2)
-
-			flag=0;
+		if strcmp(inputstem,lockstem)
+			processFile=0;
 			break
-
 		end
 
 	end
 
+	if processFile==1
+		% Make a lockfile so that we are the only one working on this image
+		save([output_dir,input_files(i).name,'-in_progress.mat'],'processFile');
 
+		% read in image
+		x=readpic([tubed_dir,input_files(i).name]);
 
-	if flag==1
-
-
-		save([output_dir,h(i).name,'-in_progress.mat'],'flag');
-
-		x=readpic([tubed_dir,h(i).name]);
-		threshold=10;
+		% threshold at arbitrary low level
 		u=zeros(size(x),'uint8');
 		u(find(x>=threshold))=1;
 
+		% Find connected components and give each island a unique index
 		[L,NUM]=bwlabeln(u,26);
 
-		disp(['Segmenting image ',h(i).name,'. Image has ',num2str(NUM),' components.'])
-
+		disp(['Segmented image ',input_files(i).name,'. Image has ',num2str(NUM),' components.'])
 
 		save([output_dir,name,'_filtered2_tubed.mat'],'x','L','NUM')
-		delete([output_dir,h(i).name,'-in_progress.mat'])
-
+		% delete lockfile
+		delete([output_dir,input_files(i).name,'-in_progress.mat'])
 	end
 
 end
