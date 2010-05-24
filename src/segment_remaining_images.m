9fb78543f231ba40ba0554eba0e8eb1069309625
Gregory Jefferis <jefferis@gmail.com>
2010-05-24 11:33:46 +0100

    Accept M-Lint suggestions in segment_remaining_images & use fullfile
    * fullfile takes care of trailing slashes in paths

--- src/segment_remaining_images.m
+++ src/segment_remaining_images.m
@@ -1,11 +1,11 @@
-function segment_remaining_images(input_dir,output_dir,threshold);
+function segment_remaining_images(input_dir,output_dir,threshold)
 
 % this script takes tubed images, thresholds them, and then segments them
 % the input files are XXXtubed.PIC the and output files are XXXtubed.mat.
 % The segmentation threshold defaults to 10
 
 if nargin < 3
-	threshold = 10
+	threshold = 10;
 end
 
 % For testing:
@@ -13,7 +13,7 @@ end
 % output_dir='~/Projects/imageProcessing/tubed_files/';
 
 % Process all input files sequentually
-input_files=dir([input_dir,'*-tubed.PIC']);
+input_files=dir(fullfile(input_dir,'*-tubed.PIC'));
 for i=1:length(input_files)
 
 	% Trim input file name up to first underscore
@@ -25,7 +25,7 @@ for i=1:length(input_files)
 	processFile=1;
 
 	% Check if we have already generated an output file for this brain
-	output_files=dir([output_dir,'*_tubed.mat']);
+	output_files=dir(fullfile(output_dir,'*_tubed.mat'));
 	for j=1:length(output_files)
 
 		n=find(output_files(j).name=='_',1,'first');
@@ -39,7 +39,7 @@ for i=1:length(input_files)
 	end
 
 	% Check if we have already generated a lockfile for this brain
-	lock_files=dir([output_dir,'*-in_progress.mat']);
+	lock_files=dir(fullfile(output_dir,'*-in_progress.mat'));
 	for j=1:length(lock_files)
 
 		n=find(lock_files(j).name=='_',1,'first');
@@ -54,23 +54,23 @@ for i=1:length(input_files)
 
 	if processFile==1
 		% Make a lockfile so that we are the only one working on this image
-		save([output_dir,input_files(i).name,'-in_progress.mat'],'processFile');
+		save(fullfile(output_dir,[input_files(i).name,'-in_progress.mat']),'processFile');
 
 		% read in image
-		x=readpic([tubed_dir,input_files(i).name]);
+		x=readpic(fullfile(input_dir,input_files(i).name));
 
 		% threshold at arbitrary low level
 		u=zeros(size(x),'uint8');
-		u(find(x>=threshold))=1;
+		u(x>=threshold)=1;
 
 		% Find connected components and give each island a unique index
 		[L,NUM]=bwlabeln(u,26);
 
 		disp(['Segmented image ',input_files(i).name,'. Image has ',num2str(NUM),' components.'])
 
-		save([output_dir,name,'_filtered2_tubed.mat'],'x','L','NUM')
+		save(fullfile(output_dir,[name,'_filtered2_tubed.mat']),'x','L','NUM');
 		% delete lockfile
-		delete([output_dir,input_files(i).name,'-in_progress.mat'])
+		delete(fullfile(output_dir,[input_files(i).name,'-in_progress.mat']));
 	end
 
 end
