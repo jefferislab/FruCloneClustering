5ddcf7e2b95e6c1e1c97e8eee146eb16748562d3
Gregory Jefferis <jefferis@gmail.com>
2010-05-24 21:11:28 +0100

    Add functions for Jlab image name handling
    * jlab_filestem returns image stem eg SAKA-1
    * matching_images returns true if there are images that match
      a given image considering only the filestem

---
+++ src/jlab_filestem.m
@@ -0,0 +1,18 @@
+function [filestem] = jlab_filestem( filename, sep )
+% jlab_filestem: return stem of image name up to first underscore (by default)
+%   sep = '_' by default
+%
+% returns the original filename if sep does not occur
+
+if nargin<2
+	sep = '_';
+end
+
+n=find(filename == sep , 1, 'first');
+if isempty(n)
+	filestem = filename;
+else
+	filestem = filename(1:n-1);
+end
+
+end
