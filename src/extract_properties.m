ae5e06392db979901265776fa6e56d09263f9205
Gregory Jefferis <jefferis@gmail.com>
2010-05-24 19:06:10 +0100

    Optimised extract_properties function
    * 10x faster when used with Bagon's ann wrapper for matlab
      (accessed by new ann_meta_wrapper function)
    * Note function signature is different
      just takes n x D matrix of D n-dimensional points
    * No longer takes ann_dir or 
      unique integer q (used to make tempfile)

--- src/extract_properties.m
+++ src/extract_properties.m
@@ -1,58 +1,50 @@
-function [alpha,vect]=extract_properties(gamma1,ann_dir,q);
+function [alpha,vect]=extract_properties(points,k)
+% extract_properties_gj - find principal eigenvector and dimensionality of points
+%
+% Usage: [alpha,vect]=extract_properties_gj(points,[k])
+%
+% Input:
+%   points - (d)x(N) matrix of d-dimensional vectors representing N points
+%   k - number of nearest neighbours to use for tangents, default = 20
+%
+% Output:
+%   alpha - dimensionality
+%   vect - principal eigenvector or tangent vector
+%
+% alpha=1 -> one-dimenionsal, alpha=0 -> isotropic). Calculated from the moment of inertia.
+% 
+% depends on ann_meta_wrapper function which provides access to
+% Arya and Mount's ANN nearest neighbour library via different
+% platform specific wrappers
 
-[total,dummy]=size(gamma1);
-alpha=zeros(1,total,'single');
-vect=zeros(3,total,'single');
+if nargin < 2
+	k = 20;
+end
 
-%ann_dir='/lmb/home/nmasse/bin/ann_1.1.2/bin/';
+[dummy npoints]=size(points);
+alpha=zeros(1,npoints,'single');
+vect=zeros(3,npoints,'single');
 
-if total>=20
-    
-    save(['DotAlphaData',num2str(q),'.txt'],'gamma1','-ascii');
-    
-    command=[ann_dir,'ann_sample -d 3 -e 0.01 -max 999999 -nn 20 -df DotAlphaData',num2str(q),'.txt -qf DotAlphaData',num2str(q),'.txt >DotAlphaOutput',num2str(q),'.txt'];
-    
-    system(command);
-    
-    [f1 f2 f3]=textread(['DotAlphaOutput',num2str(q),'.txt'],'%s %s %s');
-    
-    m1=length(f2);
-    t=zeros(1,m1,'single');
-    
-    for i=1:m1
-        t(i)=single(f2{i}(1));
-    end
-    
-    % 73 is the numeric value of 'I'
-    ind=find(t==73);
-    
-    gamma1=gamma1';
-    
-    for k=1:total
-        
-        indNN=[];
-        for j=1:20
-            indNN=[indNN 1+str2num(f2{ind(k)+j})];
-        end
-        
-        %indNN= find(sum((repmat(gamma1(:,k),1,total)-gamma1).^2)<=10^2);
-        nn=length(indNN);
-        
-        center_mass=sum(gamma1(:, indNN),2)/nn;
-        
-        inertia=sum((gamma1([1 2 3 1 2 3 1 2 3], indNN)'-repmat(center_mass([1 2 3 1 2 3 1 2 3],1)',nn,1))...
-            .*(gamma1([1 1 1 2 2 2 3 3 3], indNN)'-repmat(center_mass([1 1 1 2 2 2 3 3 3],1)',nn,1)));
-        [v1,d1]=eig(reshape(inertia,3,3));
-        
-        [d1,ind1]=sort(diag(d1),'descend');
-        alpha(k)=(d1(1)-d1(2))/sum(d1);
-        vect(:,k)=v1(:,ind1(1));
-        
-        
-    end
-    
-    
-    delete(['DotAlphaOutput',num2str(q),'.txt']);
-    delete(['DotAlphaData',num2str(q),'.txt'])
-    
+if npoints<k
+    warning ('too few points to extract properties');
+    return
+end
+
+% find k nearest neighbours for each point
+[nnidx, nndist] = ann_meta_wrapper(points,k);
+
+% iterate over each point
+for i=1:npoints
+    % nearest neighbours for this point
+    indNN=nnidx(:,i)';
+
+    center_mass=sum(points(:, indNN),2)/k;
+
+    inertia=sum((points([1 2 3 1 2 3 1 2 3], indNN)'-repmat(center_mass([1 2 3 1 2 3 1 2 3],1)',k,1))...
+        .*(points([1 1 1 2 2 2 3 3 3], indNN)'-repmat(center_mass([1 1 1 2 2 2 3 3 3],1)',k,1)));
+    [v1,d1]=eig(reshape(inertia,3,3));
+
+    [d1,ind1]=sort(diag(d1),'descend');
+    alpha(i)=(d1(1)-d1(2))/sum(d1);
+    vect(:,i)=v1(:,ind1(1));
 end
