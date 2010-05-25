642fa4d00056e3a866e71a5e8ebe8836831643ad
Gregory Jefferis <jefferis@gmail.com>
2010-05-25 01:30:14 +0100

    Streamline selection of images for processing in find_matched_dots_remaining_images
    * Use the refactored code now in jlab_filestem/matching images
    * add more comments

--- src/find_matched_dots_remaining_images.m
+++ src/find_matched_dots_remaining_images.m
@@ -14,95 +14,70 @@ properties_files=dir([input_dir,'*_properties.mat']);
 
 for i=1:length(properties_files)
     
-    n=find(properties_files(i).name=='_',1,'first');
-    current_name=properties_files(i).name(1:n-1);
+    current_image=jlab_filestem(properties_files(i).name);
         
-    % flag = 1 means that the current property file will be processed
+    % flag = 1 means that the current image property file will be 
+    %          processed from scratch
+    % flag = 2 means that some kind of update will be required
+    
     % However, there are first several checks to ensure the file is
     % not currently completed or in process
-    flag=1;
+
+    % First, check that there is no matching image in progress
+    flag = ~matching_images(current_image,...
+        [output_dir,'*-matched_points-in_progress.mat']);
     
-    h=dir([output_dir,'*-matched_points-in_progress.mat']);
-    
-    for j=1:length(h)
-        
-        n=find(h(j).name=='_',1,'first');
-        name2=h(j).name(1:n-1);
-        
-        if strcmp(name2,current_name)
-            flag=0;
-            break
-        end
-        
-    end
-    
-    h=dir([output_dir,'*matchedPoints.mat']);
-    
-    for j=1:length(h)
-        
-        n=find(h(j).name=='_',1,'first');
-        name2=h(j).name(1:n-1);
-        
-        if strcmp(name2,current_name)
-            
-            load([output_dir h(j).name],'num_images','imageList');
-            
-            if num_images==length(properties_files)
-                flag=0;
-            else
-                flag=2;
-                break
-            end
+    % Then check that there is no completed matching image
+    [match_exists matching_file_name] = matching_images(current_image,...
+        [output_dir,'*matchedPoints.mat']);
+    if match_exists
+        % if there is then load in some vars for that image
+        load([output_dir matching_file_name],'num_images','imageList');
+        % to check if all image pairs have been processed
+        if num_images==length(properties_files)
+            flag=0; % Yes, nothing to do
+        else
+            flag=2; % Some update required
         end
     end
     
     if flag==1 || flag==2
         
         if flag==1
+            % First time this image is being processed so need to construct
+            % a list of image names that it will be matched against
             imageList={};
             y=[];
         else
-            load([output_dir h(j).name])
+            % load in what we have already done
+            load([output_dir matching_file_name]);
         end
         
         save([output_dir properties_files(i).name,'-matched_points-in_progress.mat'],'flag');
         load([input_dir properties_files(i).name],'p');
         
-        p1=p;
+        p1=p; % rename p array from 1st image to avoid name clash with 2nd
         clear p
         
         [m1 n1]=size(p1.gamma2);
         
         for j=1:length(properties_files)
             
-            disp([i j])
+            disp([i j]); % We're processing image i vs image j
             
-            flag2=0;
+            short_name=jlab_filestem(properties_files(j).name,'-');
             
-            n=find(properties_files(j).name=='-',1,'first');
-            short_name=properties_files(j).name(1:n-1);
-            
-            
-            % make sure no comparaisons are repeated
+            % Process if there is no match for this image in existing list
             if ~any(strcmp(imageList,short_name))
-                flag2=1;
-            end
-            
-            if flag2==1
                 
                 y1=zeros(n1,1,'uint8');
                 
                 load([input_dir properties_files(j).name],'p');
                 
-                n=find(properties_files(j).name=='_',1,'first');
-                name=properties_files(j).name(1:n-1);
-                
-                name=name(name~='-');
-                
                 imageList{end+1}=short_name;
                 
                 %ind_union is the index of the query points (p1.gamma2)
-                %that are matched to the templaye (p.gamma2)
+                %that are matched to the template (p.gamma2)
                 ptrtree=BuildGLTree3DFEX(p.gamma2');
                 [ind_union]=compareImages_GLTree(p1,p,ptrtree);
                 DeleteGLTree3DFEX(ptrtree);
@@ -139,7 +114,7 @@ for i=1:length(properties_files)
         imageList=f1(ind);
         y=y(:,ind);
         
-        save([output_dir current_name,'_matchedPoints.mat'],'num_images','imageList','y','-v7');
+        save([output_dir current_image,'_matchedPoints.mat'],'num_images','imageList','y','-v7');
         delete([output_dir properties_files(i).name,'-matched_points-in_progress.mat'])
         
     end
