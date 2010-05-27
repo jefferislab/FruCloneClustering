520244faa854b4222166e149f3fb33a63dd42bfc
Gregory Jefferis <jefferis@gmail.com>
2010-05-27 01:51:44 +0100

    Make movies of moving dots

--- src/image_dimension_reduction.m
+++ src/image_dimension_reduction.m
@@ -77,7 +77,12 @@ for z1=1:length(connectedRegions);
 	dimension=3*ones(1,K,'single');
 
 	moveInd=1:K;
-
+    % Create a new figure, and position it
+    fig1 = figure;
+    winsize = get(fig1,'Position');
+    winsize(1:2) = [0 0];
+    mov = avifile([file_name,'-',num2str(z1),'.avi'],'fps',25,'quality',100);
+    set(fig1,'NextPlot','replacechildren');
 	% Construct nearest neighbour search tree
 	%[flanntree flannparams speedup] = flann_build_index(xcoords,struct('algorithm','kdtree','trees',8,'checks',64));
 % 	anno_xc=ann(xcoords);
@@ -178,7 +183,20 @@ for z1=1:length(connectedRegions);
 		P(moveInd)=Pnew(moveInd);
 		gamma(:,moveInd)=gammaNew(:,moveInd);
 
-	end
+        fixedPoints=setdiff(1:K,moveInd);
+        plot3(gamma(1,moveInd),gamma(2,moveInd),gamma(3,moveInd),'.',...
+            gamma(1,fixedPoints),gamma(2,fixedPoints),gamma(3,fixedPoints),'.');
+        if z==1
+            V = axis;
+        else 
+            axis(V);
+        end
+
+        F = getframe;
+        mov = addframe(mov,F);
+
+    end
+    mov=close(mov);
 
 	dots{z1}=gamma;
 	Prob{z1}=P;
