f6f2ae1cc41e23c95243c0009b72c256f6cd9dd9
Gregory Jefferis <jefferis@gmail.com>
2010-05-28 11:09:35 +0100

    Make all movie related code conditional

--- src/image_dimension_reduction.m
+++ src/image_dimension_reduction.m
@@ -82,13 +82,13 @@ for z1=1:length(connectedRegions);
     
     % Movie code
     % Create a new figure, and position it
-    fig1 = figure;
-    winsize = get(fig1,'Position');
-    winsize(1:2) = [0 0];
     if(makemovie)
+        fig1 = figure;
+        winsize = get(fig1,'Position');
+        winsize(1:2) = [0 0];
         mov = avifile([file_name,'-',num2str(z1),'.avi'],'fps',25,'quality',100);
+        set(fig1,'NextPlot','replacechildren');
     end
-    set(fig1,'NextPlot','replacechildren');
 	
     % Construct nearest neighbour search tree
 	%[flanntree flannparams speedup] = flann_build_index(xcoords,struct('algorithm','kdtree','trees',8,'checks',64));
@@ -184,19 +184,17 @@ for z1=1:length(connectedRegions);
 		gamma(:,moveInd)=gammaNew(:,moveInd);
 
         % Movie code
-        fixedPoints=setdiff(1:K,moveInd);
-        plot3(gamma(1,moveInd),gamma(2,moveInd),gamma(3,moveInd),'.',...
-            gamma(1,fixedPoints),gamma(2,fixedPoints),gamma(3,fixedPoints),'.');
-        if z==1
-            V = axis;
-        else 
-            axis(V);
-        end
         if makemovie
+            fixedPoints=setdiff(1:K,moveInd);
+            plot3(gamma(1,moveInd),gamma(2,moveInd),gamma(3,moveInd),'.',...
+                gamma(1,fixedPoints),gamma(2,fixedPoints),gamma(3,fixedPoints),'.');
+            if z==1
+                V = axis;
+            else 
+                axis(V);
+            end
             F = getframe;
             mov = addframe(mov,F);
-        else
-%             pause;
         end
     end
     % Movie code
