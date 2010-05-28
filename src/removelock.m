83d9222045540c40f419c49933df344e6c6527b9
Gregory Jefferis <jefferis@gmail.com>
2010-05-28 19:47:07 +0100

    Introduce makelock and removelock fns
    * makelock provides a simple way to make an NFS safe lockfile
    * based on an idea from Torsten Rohlfing

---
+++ src/removelock.m
@@ -0,0 +1,21 @@
+function [ status , msg] = removelock( lockfile )
+%REMOVELOCK Remove lockfile
+% Remove a lockfile - this is just deletion + warning
+% See also MAKELOCK
+
+status = false; msg = '';
+
+if ~exist(lockfile,'file')
+	msg = 'lockfile not there';
+	return;
+end
+
+delete(lockfile)
+
+if ~exist(lockfile,'file')
+	status = true;
+else
+	msg = 'unable to remove lockfile';
+end
+
+end
