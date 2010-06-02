7321ed0fc83bda7d0e76c283b0593fbac65a4a7f
Gregory Jefferis <jefferis@gmail.com>
2010-06-02 03:43:10 +0100

    Typo in compareImages_ANNTree call

--- src/find_matched_dots_remaining_images.m
+++ src/find_matched_dots_remaining_images.m
@@ -83,7 +83,7 @@ for i=1:length(properties_files)
 				%ind_union is the index of the query points (p1.gamma2)
 				%that are matched to the template (p.gamma2)
 				anno = ann(p.gamma2);
-				[ind_union] = compareImages_ANNtree(p1,p,anno);
+				[ind_union] = compareImages_ANNTree(p1,p,anno);
 				anno = close(anno);
 
 				y1(ind_union)=1;
