f8e5f717720f4a6e7b3a8f4be4189e5a81e5504a
GJAC <aostrov@mrc-lmb.cam.ac.uk>
2011-02-22 01:50:17 +0000

    Add scalefactor option to ConvertNrrdToPic
    * But Nick says this didn't actually work

--- src/convert_nrrd_to_pic.R
+++ src/convert_nrrd_to_pic.R
@@ -2,16 +2,18 @@
 cmdargs=commandArgs(trailingOnly=TRUE)
 print(cmdargs)
 
-if(length(cmdargs)!=1) {
+if(length(cmdargs)<1) {
 	warning("Using default input and output directories")
 	# Script to preprocess a directory of images 
 	NickImagesDir="/Volumes/JData/JPeople/Nick/FruCloneClustering/Reformated_images"
 } else {
 	NickImagesDir=cmdargs[[1]]
+	if(length(cmdargs)==2) scalefactor=cmdargs[[2]]
+	else scalefactor=1
 }
 cat(NickImagesDir,"\n")
 
-ConvertNrrdToPic<-function(infile,outfile,Verbose=TRUE,DryRun=FALSE,RemoveIntermediates=TRUE){
+ConvertNrrdToPic<-function(infile,outfile,Verbose=TRUE,DryRun=FALSE,RemoveIntermediates=TRUE,Scale=1){
 	if(missing(outfile)) outfile=sub("nrrd$","pic",infile)
 	# Verbose reporting
 	if(Verbose) cat("about to process image:",infile,"\n")
@@ -19,9 +21,15 @@ ConvertNrrdToPic<-function(infile,outfile,Verbose=TRUE,DryRun=FALSE,RemoveInterm
 	# open file
 	macro=c(macro,paste('open("',infile,'");',sep=""))
 	# resample, 8 bit, save as pic
-		
+	# macro=c(macro,'run("8-bit");')
+	if(Scale!=1){
+		macro=c(macro, paste(sep="",'run("Scale...", "x=',Scale,' y=',Scale,
+			' z=1.0 interpolation=Bilinear average process create title=scaled");'))
+	}
+	
+	# save as PIC file	
 	macro=c(macro,paste('run("Biorad ...", "biorad=[',outfile,']");',sep=""))
-
+	
 	tmp=paste(tempfile(),sep=".","ijm")
 	cat(paste(macro,collapse="\n"),file=tmp)
 	command=paste("fiji --headless -eval \'runMacro(\"",tmp,"\");\' -batch",sep="")
@@ -44,6 +52,6 @@ for(i in images){
 		cat(".")
 		next
 	}
-	ConvertNrrdToPic(i,picfile)
+	ConvertNrrdToPic(i,picfile,Scale=scalefactor)
 	unlink(lockfile)
 }
