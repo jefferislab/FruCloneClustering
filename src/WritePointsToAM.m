dc31c0acea9b94ad8f8cd55d6ef95a71568e3a5b
Gregory Jefferis <jefferis@gmail.com>
2011-03-13 18:56:21 +0000

    Make landmark sets compatible with my R functions

--- src/WritePointsToAM.m
+++ src/WritePointsToAM.m
@@ -7,7 +7,7 @@ fprintf(fid,'# AmiraMesh ASCII 1.0\n');
 
 nVertices=length(points(:,1));
 % write the header and then close the file
-fprintf(fid,'define Markers %d Parameters {\nContentType \"LandmarkSet\",nSets 1\n}\n',nVertices);
+fprintf(fid,'define Markers %d\nParameters {\nContentType \"LandmarkSet\",\nNumSets 1\n}\n',nVertices);
 fprintf(fid,'Markers { float[3] Coordinates } = @1\n\n');
 fprintf(fid,'@1\n');
 fclose(fid);
