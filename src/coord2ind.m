57c31f946a078c7696a1ff1647248a634ddc6b58
Gregory Jefferis <jefferis@gmail.com>
2011-03-21 16:11:28 +0000

    Fix bug in dimension checking

--- src/coord2ind.m
+++ src/coord2ind.m
@@ -14,7 +14,7 @@ function indices = coord2ind(img,voxdims,coords,aperm)
 % See also SUB2IND
 
 imsize=size(img);
-if(length(imsize)) ~= 3
+if length(imsize) ~= 3
 	error('coords2ind only handles 3d data');
 end
 	
