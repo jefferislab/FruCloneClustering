f22f9997dab18dffe43a12e0dff8e60fcdf97e73
Gregory Jefferis <jefferis@gmail.com>
2011-07-28 18:32:08 +0100

    Documentation and code style changes, no logic changes in principle

--- src/extract_properties.m
+++ src/extract_properties.m
@@ -1,34 +1,45 @@
-function [alpha,vect]=extract_properties(gamma1,ptrtree);
-
-[total,dummy]=size(gamma1);
-alpha=zeros(1,total,'single');
-vect=zeros(3,total,'single');
-
-
-if total>=20
-
-gamma1=gamma1';
-[indNN,distances]=KNNSearch3D(gamma1,gamma1,ptrtree,21);
-
-
-
-for k=1:total
-
-  
-nn=20;
-
-  center_mass=sum(gamma1(:, indNN(k,2:21)),2)/nn;
-
-        inertia=sum((gamma1([1 2 3 1 2 3 1 2 3], indNN(k,2:21))'-repmat(center_mass([1 2 3 1 2 3 1 2 3],1)',nn,1))...
-            .*(gamma1([1 1 1 2 2 2 3 3 3], indNN(k,2:21))'-repmat(center_mass([1 1 1 2 2 2 3 3 3],1)',nn,1)));
-        [v1,d1]=eig(reshape(inertia,3,3));
-
-        [d1,ind1]=sort(diag(d1),'descend');
-        alpha(k)=(d1(1)-d1(2))/sum(d1);
-        vect(:,k)=v1(:,ind1(1));
+function [alpha,vect]=extract_properties(coords,ptrtree);
+% 
+% extract_properties.m
+%
+% Function that calculates a tangent vector for each dot and an asscoiated score reflecting whether the 
+% local geometry is one-dimensional using the local moment of inertia.
+%
+% INPUTS:   
+%   coords:     x,y,z coordinates of dots in image
+%   ptrtree:    Pointer created by BuildGLTree3D.m on the coordinates of the image.
+%
+% OUTPUT:   
+%   alpha:      The difference between the first and second eigenvalues of the moment of inertia, divided 
+%               by the sum of all 3 eigenvalues. alpha=1  -> one-dimenionsal, alpha=0 -> isotropic.
+%   vect:       Tangent vector associated with each dot in image.
 
 
-end
+% The nearest number of dots used to calculate the moment of inertia
+num_dots = 20;
 
+dots_in_image = size(coords,1);
+alpha=zeros(1,dots_in_image,'single');
+vect=zeros(3,dots_in_image,'single');
+
+if dots_in_image >= num_dots
+    
+    coords=coords';
+    [indNN, dummy]=KNNSearch3D(coords,coords,ptrtree,num_dots+1);
+    
+    for k = 1:dots_in_image
+        
+        center_mass=sum(coords(:, indNN(k,2:num_dots+1)),2)/num_dots;
+        
+        inertia=sum((coords([1 2 3 1 2 3 1 2 3], indNN(k,2:21))'-repmat(center_mass([1 2 3 1 2 3 1 2 3],1)',num_dots,1))...
+            .*(coords([1 1 1 2 2 2 3 3 3], indNN(k,2:21))'-repmat(center_mass([1 1 1 2 2 2 3 3 3],1)',num_dots,1)));
+        [v,d]=eig(reshape(inertia,3,3));
+        
+        [d,ind]=sort(diag(d),'descend');
+        alpha(k)=(d(1)-d(2))/sum(d);
+        vect(:,k)=v(:,ind(1));
+        
+        
+    end  
 end
 
