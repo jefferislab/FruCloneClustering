11f5e3ba51cb32727b1d645de92124d5333f3df2
Gregory Jefferis <jefferis@gmail.com>
2011-10-19 16:13:35 +0100

    ENH: addsystempath can return current path when called without args

--- src/addsystempath.m
+++ src/addsystempath.m
@@ -6,14 +6,19 @@ function newpath = addsystempath (pathtoadd, prepend)
 % By default pathtoadd is appended to the system path. If the optional
 % argument prepend is true then it is added to the beginning of the system
 % path.  This can be used to override system binaries with user binaries.
+% When called without any parameters, the current system path is returned
 %
 % See also getenv, setenv
 
+curpath = getenv('PATH');
+
+if nargin < 1
+	newpath = curpath;
+	return
+end
 if nargin < 2
 	prepend = false;
 end
-
-curpath = getenv('PATH');
 
 % FIXME - should really split system path into each separate component and
 % then check for exact match
