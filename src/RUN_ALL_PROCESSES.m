9a808583350e4221457be87db6a84a3184126c9d
Gregory Jefferis <jefferis@gmail.com>
2011-10-29 22:35:44 +0100

    Move setting of directories into a separate script

--- src/RUN_ALL_PROCESSES.m
+++ src/RUN_ALL_PROCESSES.m
@@ -13,34 +13,7 @@
 % e.g. addsystempath('/Applications/IGSRegistrationTools/bin/');
 
 %%%% Input and output directories
-
-src_dir = fileparts(which('RUN_ALL_PROCESSES'));
-
-root_dir = fileparts(src_dir);
-
-original_images_dir=fullfile(root_dir,'Source_images');
-
-processed_images_dir=fullfile(root_dir,'Processed_images');
-
-segmented_images_dir=fullfile(root_dir,'Segmented_images');
-
-dimension_reduced_dir=fullfile(root_dir,'dimension_reduced_images');
-
-reformated_points_dir=fullfile(root_dir,'Reformated_points');
-
-reformated_images_dir=fullfile(root_dir,'Reformated_images');
-
-properties_dir=[root_dir 'image_properties'];
-
-mask_dir = fullfile(root_dir,'masks');
-
-matched_dots_dir=fullfile(root_dir,'matched_points');
-
-% Directory with the warp registration data (ie directory containing IS2_SAKW9-1_01_warp_m0g80c8e1e-1x26r4.list)
-registration_dir=fullfile(root_dir,'Registration');
-
-% Directory of gregxform unix command to refomat the coordinates
-bin_dir=fullfile(root_dir,'bin');
+Set_Masse_Dirs
 
 %%%%%
 
