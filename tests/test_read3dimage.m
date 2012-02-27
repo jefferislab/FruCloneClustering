bf17c716d7b441cd2738373b65eade550bb4b28c
Gregory Jefferis <jefferis@gmail.com>
2012-02-27 19:53:31 +0000

    warn if unable to locate files for tests
    * and look in a more specific place (off path) for nerd

--- tests/test_read3dimage.m
+++ tests/test_read3dimage.m
@@ -4,17 +4,22 @@ function test_suite = testCoords
 initTestSuite;
 
 function test_nrrd
-filename=which('7x6x3-neworigin.nhdr');
-if ~isempty(filename)
+nrrdiodir=fileparts(which('isnrrd'));
+if ~isempty(nrrdiodir)
+	filename=fullfile(nrrdiodir,'tests','7x6x3-neworigin.nhdr');
 	[x,voxdims,origin] = read3dimage(filename);
-	assertEqual(voxdims, [0.5 0.4 1.0])
-	assertEqual(origin, [10 20 15])
+	assertEqual(voxdims, [0.5 0.4 1.0]);
+	assertEqual(origin, [10 20 15]);
+else
+	warning('unable to locate 7x6x3-neworigin.nhdr for test');
 end
 
 function test_pic
 filename=which('7x6x3-origin.PIC');
 if ~isempty(filename)
 	[x,voxdims,origin] = read3dimage(filename);
-	assertEqual(voxdims, [0.5 0.4 1.0])
-	assertEqual(origin, [0 0 0])
+	assertEqual(voxdims, [0.5 0.4 1.0]);
+	assertEqual(origin, [0 0 0]);
+else
+	warning('unable to locate 7x6x3-origin.PIC for test');
 end
