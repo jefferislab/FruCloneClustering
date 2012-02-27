aba3bd323db46d00e939047bcff73872b5db5ca7
Gregory Jefferis <jefferis@gmail.com>
2012-02-27 19:32:15 +0000

    Just test for origin/voxdims as row vectors

--- tests/test_read3dimage.m
+++ tests/test_read3dimage.m
@@ -7,14 +7,14 @@ function test_nrrd
 filename=which('7x6x3-neworigin.nhdr');
 if ~isempty(filename)
 	[x,voxdims,origin] = read3dimage(filename);
-	assertEqual(voxdims, [0.5 0.4 1.0]')
-	assertEqual(origin, [10 20 15]')
+	assertEqual(voxdims, [0.5 0.4 1.0])
+	assertEqual(origin, [10 20 15])
 end
 
 function test_pic
 filename=which('7x6x3-origin.PIC');
 if ~isempty(filename)
 	[x,voxdims,origin] = read3dimage(filename);
-	assertEqual(voxdims, [0.5 0.4 1.0]')
-	assertEqual(origin, [0 0 0]')
+	assertEqual(voxdims, [0.5 0.4 1.0])
+	assertEqual(origin, [0 0 0])
 end
