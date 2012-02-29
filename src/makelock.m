1e3380584e6d7c19a709b89d851f5dc78b0ae238
Gregory Jefferis <jefferis@gmail.com>
2012-02-29 20:05:52 +0000

    Replace makelock from oldmaster
    * extra functionality includes verbose flag
    * and locking with gridengine job id
    * and Octave compatible

--- src/makelock.m
+++ src/makelock.m
@@ -1,32 +1,46 @@
-function [ success ] = makelock( lockfile, lockmsg, createDirectories )
+function [ success ] = makelock( lockfile, lockmsg, verbose, createDirectories )
 %MAKELOCK Make a lockfile (NFS safe in principle)
 %  Make a lockfile containing lockmsg
-%  lockmsg defaults of matlab's tempname preceded (on unix) by hostname
+%  lockmsg defaults to matlab's tempname preceded (on unix) by hostname
+%  When running as a grid engine job defaults to hostname:JOB_ID
+%  verbose = false by default
 %  Create director(y|ies) containing lockfile if required
 %  Returns 1 for success, 0 otherwise
 %
 %  See also REMOVELOCK
 
-if(nargin<3)
+if nargin<3
+    verbose=false;
+end
+if(nargin<4)
 	createDirectories = true;
 end
-if(nargin<2)
+
+if nargin<2 || isempty(lockmsg)
 	if isunix
 		% get this hostname as additional protection against
 		% collision of unique identifier
-		[~, hostname]=system('hostname');
-		% note strcat removes trailing whitespace from hostname
-		lockmsg = strcat(hostname,tempname);
+		[status, hostname]=system('hostname');
+		% Gridengine sets this
+		jobid=getenv('JOB_ID');
+		if ~isempty(jobid)
+			lockmsg=strcat(strtrim(hostname),':',jobid);
+		else
+			% note strtrim removes trailing whitespace from hostname
+			lockmsg = strcat(strtrim(hostname),':',tempname);
+		end
+
 	else
 		lockmsg = tempname;
 	end
 end
 
-[lockdir,~,~] = fileparts(lockfile);
+lockdir = fileparts(lockfile);
 if ~isempty(lockdir)
 	if ~exist(lockdir,'dir')
 		if createDirectories
 			% nb mkdir is recursive
+            if verbose, disp(['making directory ' lockdir]),end
 			mkdir(lockdir);
 		else
 			error('Lock directory for lockfile %s does not exist',lockfile);
@@ -39,6 +53,7 @@ success = false;
 
 if exist(lockfile,'file')
 	% somebody else already made the lockfile
+    if verbose, disp(['somebody already made the lockfile ' lockfile]),end
 else
 	% write a (unique) message to lockfile
 	fid = fopen(lockfile,'a');
@@ -49,8 +64,10 @@ else
 	firstLine = fgetl(fid);
 	% check if the first line contains our (unique) message
 	if strcmp(firstLine,lockmsg)
+        if verbose, disp(['successfully made lockfile ' lockfile]),end
 		success = true;
-	else
+    else
+        if verbose, disp(['someone else already made ' lockfile]),end
 		% neck and neck race which we lost by a head
 		% Multiple processes wrote to same lockfile and we weren't first
 	end
