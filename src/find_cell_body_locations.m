a30c4fca53d366c17575f88f8c7a110f5fbc6460
Gregory Jefferis <jefferis@gmail.com>
2012-02-29 03:02:24 +0000

    find_cell_body_locations - improve documentation some code tidying
    * no logic changes, though add threshold parameter (defaults to 0.5)

--- src/find_cell_body_locations.m
+++ src/find_cell_body_locations.m
@@ -1,25 +1,40 @@
-function cell_body_coords = find_cell_body_locations(image_file)
-
-% image_file is a PIC file of the reformated (and masked) image
-% outputs a list of coordinates of the filtered image that are above
-% threshold. These coordinates are the likely locations of cell bodies.
-
+function cell_body_coords = find_cell_body_locations(image_file, threshold)
+% Find cell body locations based on reformatted (and masked) image
+%
+% Input:
+% image_file - reformated (and masked) image
+%
+% Output:
+% cell_body_coords - likely location of cell bodies (uint16)
+% threshold        - fraction of maximum intensity (default = 0.5)
+%
+% (Masked) image data is convolved with a 2x2x2 3D boxcar filter and then
+% thresholded up to some feaction of the maximum intensity. These
+% coordinates are the likely locations of cell bodies.
+% 
+% The mask used to generate the input images is the inverse of the neuropil
+% mask used for neuronal projections since cell bodies in flies (insects)
+% are located in a cortex on the outside of the brain
+%
+% See also 
+if nargin < 2
+	threshold = 0.5;
+end
 f = ones(2,2,2,'uint8');
 
-x = readpic(image_file);
-
-metadata = impicinfo(image_file);
-
-voxdims = metadata.Delta;
+[x, voxdims] = read3dimage(image_file);
 
 image_size = size(x);
 
 y = convn(x,f,'same');
 
-max_intensity = double(max(max(max(y))));
+max_intensity = double(max(y(:)));
 
-ind = find(y > 0.5 * max_intensity);
+ind = find(y > threshold * max_intensity);
 
-coords = ind2coord(image_size,ind,voxdims,[2 1 3]); % changed from [1 2 3] to [2 1 3], NYM Oct 29, 2011
+% note use of [2 1 3] axis permutation to cope with image data in matlab's
+% standard form (help ind2coord for details)
+coords = ind2coord(image_size,ind,voxdims,[2 1 3]);
 
+% TODO - Nick, why uint16 here?
 cell_body_coords = uint16(coords);
