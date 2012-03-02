9cbee77120b2bfa9fbcdf882ecaa5c34619b297b
Gregory Jefferis <jefferis@gmail.com>
2012-03-02 09:26:33 +0000

    Implement scaling in 3D via ImgLib
    * a fair bit of faffing around because
      1) Scale3D does not handle calibration of output stack
      2) The resultant virtual stack is a float in imglib1
         (but not 2) and this must be copied and converted to 8 bit.

--- src/scaleandfilter.py
+++ src/scaleandfilter.py
@@ -15,7 +15,7 @@ Options:
 """
 
 __author__ = "Gregory Jefferis"
-__copyright__ = "Copyright (c) 2010 Gregory Jefferis"
+__copyright__ = "Copyright (c) 2010-12 Gregory Jefferis"
 __license__ = "GPL >= v2"
 
 import sys
@@ -29,34 +29,60 @@ if sys.version_info > (2, 4):
 import os
 from features import TubenessProcessor
 
+# nb soon script.imglib -> net.imglib2.script
+from script.imglib import ImgLib
+from script.imglib.algorithm import Scale3D
+
 def scaleandfilter(infile,outfile,scalex,scaley,scalez,anisofilter):
 	
 	print ("infile is: "+infile)
-	
 	imp = Opener().openImage(infile)
 	print imp
 	print "scalex = %f; scaley = %f ; scalez = %f" % (scalex,scaley,scalez)
-
+	
 	# Rescale
-	ip = imp.getProcessor()
-	ip.setInterpolate(True)
-	sp = StackProcessor(imp.getStack(),ip);
-	sp2=sp.resize(int(round(ip.width * scalex)), int(round(ip.height *scaley)));
-	imp.setStack(imp.getTitle(),sp2);
-	
 	cal = imp.getCalibration()
-	cal.pixelWidth /= scalex
-	cal.pixelHeight /= scaley
-
-	IJ.run(imp, "8-bit","")
+	iml = ImgLib.wrap(imp)
+	scaledimg = Scale3D(iml, scalex, scaley, scalez)
+	imp2=ImgLib.wrap(scaledimg)
 	
-	outtif=infile+"-filtered.tif"
+	# find range of pixel values for scaled image
+	from mpicbg.imglib.algorithm.math import ComputeMinMax
+	# (for imglib2 will be: net.imglib2.algorithm.stats)
+	minmax=ComputeMinMax(scaledimg)
+	minmax.process()
+	(min,max)=(minmax.getMin().get(),minmax.getMax().get())
+	# Make a copy of the stack (converting to 8 bit as we go)
+	stack = ImageStack(imp2.width, imp2.height)
+	print "min = %e, max =%e" % (min,max)
+	for i in xrange(1, imp2.getNSlices()+1):
+		imp2.setSliceWithoutUpdate(i)
+		ip=imp2.getProcessor()
+		# set range
+		ip.setMinAndMax(min,max)
+		stack.addSlice(str(i), ip.convertToByte(True))
+	
+	# save copy of calibration info
+	cal=imp.getCalibration()
+	# close original image
+	imp.close()
+	# make an image plus with the copy
+	scaled = ImagePlus(imp2.title, stack)
+	
+	# Deal with calibration info which didn't seem to come along for the ride
+	cal.pixelWidth/=scalex
+	cal.pixelHeight/=scaley
+	cal.pixelDepth/=scalez
+	scaled.setCalibration(cal)
+	print "dx = %f; dy=%f; dz=%f" % (cal.pixelWidth,cal.pixelHeight,cal.pixelDepth)
+	
 	intif=infile+".tif"
+	outtif=infile+"-filtered.tif"
 	if anisofilter.upper() != 'FALSE':
 		print("saving input file as "+intif)
-		f=FileSaver(imp)
+		f=FileSaver(scaled)
 		f.saveAsTiffStack(intif)
-		imp.close()
+		scaled.close()
 		# anisotropic filtering
 		anisopts="-scanrange:10 -tau:2 -nsteps:2 -lambda:0.1 -ipflag:0 -anicoeff1:1 -anicoeff2:0 -anicoeff3:0"
 		anisopts=anisopts+" -dx:%f -dy:%f -dz:%f" % (cal.pixelWidth,cal.pixelHeight,cal.pixelDepth)
@@ -74,7 +100,7 @@ def scaleandfilter(infile,outfile,scalex,scaley,scalez,anisofilter):
 	# Hessian (tubeness)
 	print("Running tubeness")
 	tp=TubenessProcessor(1.0,False)
-	result = tp.generateImage(imp)
+	result = tp.generateImage(scaled)
 	IJ.run(result, "8-bit","")
 	# Save out file
 	fileName, fileExtension = os.path.splitext(outfile)
