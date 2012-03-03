2fec43b0b6b6e21521b6c5c9d794c25bd4614a60
Gregory Jefferis <jefferis@gmail.com>
2012-03-03 16:02:41 +0000

    Turn Set_Masse_Dirs into function that can include subduer
    * idea is we can do different runs with data going into different 
      subdirs.

--- src/Set_Masse_Dirs.m
+++ src/Set_Masse_Dirs.m
@@ -1,29 +1,48 @@
+function Set_Masse_Dirs(subdir)
+% SET_MASSE_DIRS   Set up the directories used in later processing stages
+% 
+% Usage: Set_Masse_Dirs(subdir)
+%
+% Optionally include a subdir for the data specific directories so that we
+% can keep multiple runs with different parameters organised and separate
+% 
+% Created by Gregory Jefferis on 2012-03-03.
+% Copyright (c)  MRC LMB. All rights reserved.
+
+if nargin<1
+	subdir='';
+end
+
 % Script to set standard location of directories used throughout project
 
 src_dir = fileparts(which('RUN_ALL_PROCESSES'));
 
+assignin('base','src_dir' , src_dir);
+
 root_dir = fileparts(src_dir);
+assignin('base','root_dir', root_dir);
 
-original_images_dir=fullfile(root_dir,'Source_images');
+assignin('base','original_images_dir',fullfile(root_dir,'Source_images',subdir));
 
-processed_images_dir=fullfile(root_dir,'Processed_images');
+assignin('base','processed_images_dir',fullfile(root_dir,'Processed_images',subdir));
 
-segmented_images_dir=fullfile(root_dir,'Segmented_images');
+assignin('base','segmented_images_dir',fullfile(root_dir,'Segmented_images',subdir));
 
-dimension_reduced_dir=fullfile(root_dir,'dimension_reduced_images');
+assignin('base','dimension_reduced_dir',fullfile(root_dir,'dimension_reduced_images',subdir));
 
-reformated_points_dir=fullfile(root_dir,'Reformated_points');
+assignin('base','reformated_points_dir',fullfile(root_dir,'Reformated_points',subdir));
 
-reformated_images_dir=fullfile(root_dir,'Reformated_images');
+assignin('base','reformated_images_dir',fullfile(root_dir,'Reformated_images',subdir));
 
-properties_dir=[root_dir 'image_properties'];
+assignin('base','properties_dir',fullfile(root_dir,'image_properties',subdir));
 
 mask_dir = fullfile(root_dir,'masks');
 
-matched_dots_dir=fullfile(root_dir,'matched_points');
+assignin('base','matched_dots_dir',fullfile(root_dir,'matched_points',subdir));
 
 % Directory with the warp registration data (ie directory containing IS2_SAKW9-1_01_warp_m0g80c8e1e-1x26r4.list)
 registration_dir=fullfile(root_dir,'Registration');
 
 % Directory of gregxform unix command to refomat the coordinates
 bin_dir=fullfile(root_dir,'bin');
+end %  function
