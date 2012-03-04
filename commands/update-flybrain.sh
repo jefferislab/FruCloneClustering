6df02c1f3566bad47bedd9c923ff6e25d540810d
Gregory Jefferis <jefferis@gmail.com>
2012-03-04 13:43:56 +0000

    fixes to update scipt
    no .zip file
    make sure Reformated_points is dir

--- commands/update-flybrain.sh
+++ commands/update-flybrain.sh
@@ -3,12 +3,12 @@
 # script to update zip files on flybrain
 
 cd /lmb/home/jefferis/projects/frucc/FruCloneClustering/Reformated_points/
-dirs=`find . -type d`
+dirs=`find . -type d ! -name "."`
 echo $dirs
 for x in $dirs; do
   #statements
   echo $x
   zip -u -0 "$x.zip" "$x"/*
-  rsync -avP "$x.zip" lmbfly:/var/www/html/Masse2011/FruCloneClustering/Reformated_points
+  rsync -avP "$x.zip" lmbfly:/var/www/html/Masse2011/FruCloneClustering/Reformated_points/
 done
 # rsync -avnP --include="*.zip" --exclude="*.*" Reformated_points lmbfly:/var/www/html/Masse2011
