9c3cf9e7d4550381a153099004ce041d01f30c91
Gregory Jefferis <jefferis@gmail.com>
2012-03-04 13:35:48 +0000

    Add script to send zipped results to flybrain for Nick to download

---
+++ commands/update-flybrain.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# script to update zip files on flybrain
+
+cd /lmb/home/jefferis/projects/frucc/FruCloneClustering/Reformated_points/
+dirs=`find . -type d`
+echo $dirs
+for x in $dirs; do
+  #statements
+  echo $x
+  zip -u -0 "$x.zip" "$x"/*
+  rsync -avP "$x.zip" lmbfly:/var/www/html/Masse2011/FruCloneClustering/Reformated_points
+done
+# rsync -avnP --include="*.zip" --exclude="*.*" Reformated_points lmbfly:/var/www/html/Masse2011
